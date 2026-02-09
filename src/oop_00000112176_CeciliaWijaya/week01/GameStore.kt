36e5243fd664d5e8bc2a4a4f5d34b3e1e2550a03
Linuell <ceciliawijaya06@gmail.com>
2026-02-09 22:28:50 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000112176_CeciliaWijaya/week01/GameStore.kt
+++ src/oop_00000112176_CeciliaWijaya/week01/GameStore.kt
@@ -3,4 +3,8 @@ package oop_00000112176_CeciliaWijaya.week01
 fun main() {
     val gameTitle = "SteamKW"
     val price = 750000
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int) =
+    if (price >500000) price *80 / 100 else price * 90/ 100
