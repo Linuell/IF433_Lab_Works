b824e96c980c266f70a040c5d5436a3839dd46df
Linuell <ceciliawijaya06@gmail.com>
2026-02-10 21:51:12 +0700

    week02: (task 1) implement library input system

---
+++ src/oop_00000112176_CeciliaWijaya/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_00000112176_CeciliaWijaya.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
