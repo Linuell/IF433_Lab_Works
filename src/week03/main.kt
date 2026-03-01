ada92390ee2663a01d991939827f0dcc0cf29482
Linuell <ceciliawijaya06@gmail.com>
2026-03-01 21:30:13 +0700

    week03: (task 2) implement Player testing in main

--- src/week03/main.kt
+++ src/week03/main.kt
@@ -11,7 +11,7 @@ fun main () {
 
     println("Pajak yang harus dibayar: ${e.tax}");
 
-//tugas
+//tugas1
     val sword = Weapon("Dragon Slayer")
 
     sword.damage = -50
@@ -20,4 +20,11 @@ fun main () {
     println("Weapon: ${sword.name}")
     println("Damage: ${sword.damage}")
     println("Tier: ${sword.tier}")
+
+    //tugas 2
+    val player = Player("Cecil")
+
+    player.addXp(50)
+
+    player.addXp(60)
 }
