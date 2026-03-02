d0c90504d922d8a163966954b4da62325f662b2c
Linuell <ceciliawijaya06@gmail.com>
2026-03-02 18:29:14 +0700

    week04: implement super keyword in Car accelerate

--- src/week04/Car.kt
+++ src/week04/Car.kt
@@ -1,7 +1,8 @@
 package week04
 
 open class Car (brand: String, val numberOfDoors: Int) : Vehicle(brand) {
-    override fun honk () {
-        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    override fun accelerate () {
+        super.accelerate() // Memanggil logika penambahan kecepatan milik Parent
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
     }
 }
