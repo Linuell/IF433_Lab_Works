5c00ea0419f46ec28ead9b090dc892d907bb3bd0
Linuell <ceciliawijaya06@gmail.com>
2026-03-10 20:47:26 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/Week06/SmartSpeaker.kt
+++ src/Week06/SmartSpeaker.kt
@@ -1,4 +1,19 @@
 package Week06
 
-class SmartSpeaker {
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable { // Mewarisi SmartDevice dan Switchable [cite: 154]
+
+    override fun turnOn() {
+        println("Speaker [$name] sekarang aktif.")
+    }
+
+    override fun turnOff() {
+        println("Speaker [$name] standby.")
+    }
+
+    fun playMusic(song: String) { // Fungsi spesifik [cite: 155]
+        println("Memutar lagu $song dari Spotify.")
+    }
 }
