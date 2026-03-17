b7f4e21054a27b54c48d46e8298cfd2575b91b7f
Linuell <ceciliawijaya06@gmail.com>
2026-03-17 15:15:08 +0700

    week07: implement companion object as factory in NetworkClient

--- src/Week07/DatabaseManager.kt
+++ src/Week07/DatabaseManager.kt
@@ -1,4 +1,10 @@
 package Week06.Week07
 
 object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect(){
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
 }
--- src/Week07/Main.kt
+++ src/Week07/Main.kt
@@ -1,4 +1,5 @@
 package Week06.Week07
 
 class Main {
+    val client = NetworkClient ("https://api.umn.ac.id")
 }
