fabca6b972811f7ba5a8794bd8067581954818c7
Linuell <ceciliawijaya06@gmail.com>
2026-04-14 15:15:12 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/week08/UserProfile.kt
@@ -0,0 +1,5 @@
+package Week06.week08
+
+// name adalah Non-null(wajib), email dan phone adalah nullable
+class UserProfile (val name: String, val email: String?, val phone: String? = null //Default argument null
+)
