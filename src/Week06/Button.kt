b7730052c0c566ff4fc1e6793a88440f97da0aa2
Linuell <ceciliawijaya06@gmail.com>
2026-04-14 16:06:09 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/Week06/Button.kt
+++ src/Week06/Button.kt
@@ -1,6 +1,6 @@
 package Week06
 
-class Button(override val name: String): Clickable {
+class IconButton(override val name: String): Clickable {
     override fun click() {
         println("Tombol '$name' berhasi; diklik")
     }
