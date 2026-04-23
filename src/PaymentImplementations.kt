e675ff3dc84d4d9d71ab635c42893c0663be67f7
Linuell <ceciliawijaya06@gmail.com>
2026-04-23 20:19:56 +0700

    week09: create immutable and mutable list

--- src/PaymentImplementations.kt
+++
@@ -1,8 +0,0 @@
-package Week06
-
-class Gopay :  PaymentMethod{
-    override fun pay(amount: Double) { println("Processing Rp$amount via Gopay server")}
-    }
-class CreditCard :  PaymentMethod{
-    override fun pay(amount: Double) { println("Contacting Bank for Rp$amount")}
-}
