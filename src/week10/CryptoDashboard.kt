f40921af63a70c5ec1b55b200927515e22aa680f
Linuell <ceciliawijaya06@gmail.com>
2026-04-28 15:37:44 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/week10/CryptoDashboard.kt
+++ src/week10/CryptoDashboard.kt
@@ -4,4 +4,9 @@ fun main() {
 
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 5.70))
+    coinRepo.add(Coin("USDT", 1200.00))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
