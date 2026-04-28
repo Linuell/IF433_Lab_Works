ec3d7cdcc87b9e9ef3224ab6633e3b403d29e46e
Linuell <ceciliawijaya06@gmail.com>
2026-04-28 15:39:16 +0700

    week10: (task) initialize and populate transaction repository

--- src/week10/CryptoDashboard.kt
+++ src/week10/CryptoDashboard.kt
@@ -15,4 +15,10 @@ fun main() {
     response.data.forEach {
         println("Coin: ${it.name}, Balance: ${it.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+
+    txRepo.add(Transaction("TX001", 500000.0))
+    txRepo.add(Transaction("TX002", 250000.0))
+
 }
