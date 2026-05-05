3364ffd7c90a80490a08896d83e5b6962f949677
Linuell <ceciliawijaya06@gmail.com>
2026-05-05 14:24:20 +0700

    week11: (task) implement device search with let

--- src/week11/Main.kt
+++ src/week11/Main.kt
@@ -39,4 +39,13 @@ fun main() {
     homeDevices.add(acUnit)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
+
+
 }
+
+
